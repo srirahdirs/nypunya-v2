c8c65834ded120abb3597a123c88011ceb06c1dd
Sridhar Vellingiri <sri.rahdirs@gmail.com>
2025-06-27 16:32:01 +0530

    Faq

--- src/component/ServiceComponent/RhinoplastyComponent/index.jsx
+++ src/component/ServiceComponent/RhinoplastyComponent/index.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
-import Banner from "../RhinoplastyComponent/Banner"
-import Rhinoplasty from "../RhinoplastyComponent/Rhinoplasty"
-import GalleryComponent from './GalleryComponent'
-import ConsultationForm from '../../FormsComponent/ConsultationForm'
-import FaqRhinoplasty from './FaqRhinoplasty'
-import { useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import Banner from "../RhinoplastyComponent/Banner";
+import Rhinoplasty from "../RhinoplastyComponent/Rhinoplasty";
+import GalleryComponent from './GalleryComponent';
+import ConsultationForm from '../../FormsComponent/ConsultationForm';
+import FaqRhinoplasty from './FaqRhinoplasty';
+
 const RhinoplastyComponent = () => {
   const location = useLocation();
   const { scrollTo } = location.state || {};
   const faqRef = useRef(null);
-  useEffect(() => {
+
+  useLayoutEffect(() => {
+    // Give time for nested components to mount
     if (scrollTo === 'faqs' || window.location.hash === '#faq-section') {
       setTimeout(() => {
         if (faqRef.current) {
           faqRef.current.scrollIntoView({ behavior: 'smooth' });
         }
-      }, 300);
+      }, 500); // Adjust this if needed
     }
   }, [scrollTo]);
 
@@ -30,7 +32,7 @@ const RhinoplastyComponent = () => {
       </div>
       <ConsultationForm />
     </>
-  )
-}
+  );
+};
 
-export default RhinoplastyComponent
+export default RhinoplastyComponent;
